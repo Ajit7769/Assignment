6fb5392dcd43bdf430f48af9f1e7dc3692d337ad
Ajit7769 <asarwade2017@gmail.com>
2024-05-25 22:18:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Card from './Componant/Card'
+import About from './Componant/About'
+import Contact from './Componant/Contact'
+import SocialLinks from './Componant/SocialLinks'
+import ProjectLink from './Componant/ProjectLink'
+import { BrowserRouter } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BrowserRouter>
+     <div className='mt-[3%] w-[100%]'>
+         <div><Card /></div>
+         <div className='max-lg:mt-1'><About /></div>
+         <div className='max-lg:mt-1'><Contact /></div>
+         <div><SocialLinks /></div>
+         <div><ProjectLink /></div>
+     </div>
+     </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
